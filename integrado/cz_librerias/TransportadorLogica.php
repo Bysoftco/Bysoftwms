278a68dfec31834ab0d86af2a7a887330d00c364
soportebysoft <soporte@bysoft.us>
2017-11-22 16:23:51 -0500

    modulo tranportador con crear sin filtro

---
+++ integrado/cz_librerias/TransportadorLogica.php
@@ -0,0 +1,102 @@
+<?php
+require_once("ReporteExcel.php");
+require_once("TransportadorDatos.php");
+require_once("TransportadorPresentacion.php");
+
+class TransportadorLogica  {
+  		var $datos;
+		var $pantalla;
+		
+        function TransportadorLogica () {
+			    $this->datos = &new Transportador();
+                $this->pantalla = &new TransportadorPresentacion($this->datos);
+            }
+			
+			
+		function filtro($arregloDatos){
+                $arregloDatos[mostrar]          =1;
+                $arregloDatos[plantilla]        ='transportadorFiltro.html';
+                $arregloDatos[thisFunction]     ='filtro';
+                $this->pantalla->setFuncion($arregloDatos,&$this->datos);
+            }
+            
+             function titulo($arregloDatos){
+                if(!empty($arregloDatos[cliente])) {
+                   $arregloDatos[titulo] ="Cliente ".$arregloDatos[cliente];
+                }
+                
+                if(!empty($arregloDatos[fecha_inicio])) {
+                   $arregloDatos[titulo] =" Desde ".$arregloDatos[fecha_inicio]." Hasta.$arregloDatos[fecha_fin]";
+                }
+             }
+            
+            function getSubpartida($arregloDatos){
+                $this->titulo(&$arregloDatos);
+                $arregloDatos[mostrar]          =1;
+                $arregloDatos[plantilla]        ='SubpartidaSubpartidaInventario.html';
+                $arregloDatos[thisFunction]     ='inventario';
+                $this->pantalla->setFuncion($arregloDatos,&$this->datos);
+                
+            }
+			function getUnaSubpartida($arregloDatos){
+                $arregloDatos[mostrar] = 1;
+    			$arregloDatos[plantilla] = 'transportadorFormulario.html';
+   				$arregloDatos[thisFunction] = 'getUnaSubpartida';
+    			$this->pantalla->setFuncion($arregloDatos,&$this->datos);
+                
+            }
+			function updateSubpartida($arregloDatos){
+              $this->datos->updateSubpartida($arregloDatos); 
+                
+            }
+			
+		function findSupartida($arregloDatos) {
+		$unaConsulta = new Subpartida();
+
+		$unaConsulta->findCliente($arregloDatos);
+		$arregloDatos[q] = strtolower($_GET["q"]);
+
+		while($unaConsulta->fetch()) {
+			$nombre = utf8_encode(trim($unaConsulta->descripcion));
+			echo "$nombre|$unaConsulta->subpartida|	$nombre\n";
+		}
+		if($unaConsulta->N == 0) {
+			echo "No hay Resultados|0\n";
+		}
+	}
+
+            function generaExcel ($arregloDatos) 
+            {
+                $arregloDatos[excel]=1;
+                $arregloDatos['titulo']='Subpartida '.$arregloDatos[titulo];
+                $arregloDatos['sql']=$this->datos->getInventario($arregloDatos);
+               // echo $arregloDatos['sql'];die();
+                $unExcel = new SubpartidaExcel($arregloDatos);
+                $unExcel->generarExcel();
+
+            } 
+             function maestro($arregloDatos) {
+			 	 $this->pantalla->maestro($arregloDatos);
+			} 
+			function getListado($arregloDatos) {
+				                //$this->titulo(&$arregloDatos);
+                $arregloDatos[mostrar]          =1;
+                $arregloDatos[plantilla]        ='transportadorListado.html';
+                $arregloDatos[thisFunction]     ='getListado';
+                $this->pantalla->setFuncion($arregloDatos,&$this->datos);
+			 	 //$this->pantalla->maestro($arregloDatos);
+			}
+			function getFormaNueva($arregloDatos){
+				$arregloDatos[plantilla]        ='transportadorFormNueva.html';
+                $arregloDatos[thisFunction]     ='getFormaNueva';
+                $this->pantalla->setFuncion($arregloDatos,&$this->datos);
+				
+			}
+			
+			function addTransportador($arregloDatos){
+				$this->datos->addTransportador($arregloDatos);
+			}
+          
+	}		
+  	
+?>
