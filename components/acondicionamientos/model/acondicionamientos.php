3941fbe40a65c2fb1a541159160904b782a8397c
soportebysoft <soporte@bysoft.us>
2018-02-03 16:42:47 -0500

    correccion acondicimiento varias ordenes

--- components/acondicionamientos/model/acondicionamientos.php
+++ components/acondicionamientos/model/acondicionamientos.php
@@ -170,12 +170,12 @@ class acondicionaDatos extends BDControlador {
         $arreglo['cif'] = $arreglo['val_unit'] * $arreglo['cantidad_naci'];
         $arreglo['fob_nonac'] = $arreglo['val_unit'] * $arreglo['cantidad_nonac'];
         //Registra cantidad acondicionada en inventario_movimientos
-        $query = "UPDATE inventario_movimientos SET cantidad_naci = cantidad_naci+$arreglo[rechazadas],
+        $query = "UPDATE inventario_movimientos SET cantidad_naci = cantidad_naci+$arreglo[rechazadas]+$arreglo[devueltas],
                     cantidad_nonac = cantidad_nonac+($arreglo[rechazadas]+$arreglo[devueltas]), peso_naci = $arreglo[peso_naci]*-1,
                     peso_nonac = $arreglo[peso_nonac]*-1, cif = $arreglo[cif]*-1, fob_nonac = $arreglo[fob_nonac]*-1
                   WHERE cod_maestro = $arreglo[codigo_operacion] AND tipo_movimiento = 16 AND inventario_entrada=$arreglo[inventario_entrada]";
         
-		echo $query;
+		//echo $query;
 		$db->query($query);
         break;
       }
