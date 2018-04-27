81a86c2239e60098fef7dda50b752264d3c97448
fsalomp <fsalom@bysoft.us>
2018-04-27 00:08:55 -0500

    actualización movimientos

--- components/acondicionamientos/views/tmpl/detalleOrdenAcondicionamiento.php
+++ components/acondicionamientos/views/tmpl/detalleOrdenAcondicionamiento.php
@@ -54,7 +54,15 @@
     #tabla_packinglist td:hover {
       background-color: #fff;
     }
-    .saltarpagina { page-break-before: always; } /* page-break-after también funciona */      
+    .saltarpagina { page-break-before: always; } /* page-break-after también funciona */
+		#num_pagina:after { 
+			counter-increment: page;
+			content: counter(page)
+		}
+		#tot_pagina:after { 
+			counter-increment: pages;
+			content: counter(pages)
+		}
   </style>
 </head>
 <body>
@@ -80,9 +88,9 @@
     <tr>
       <td style="text-align:center;">{codigo_reporte}</td>
       <td></td>
-      <td style="text-align:center;">1</td>
+      <td style="text-align:center;"><span>1</span></td>
       <td style="text-align:center;">de</td>
-      <td style="text-align:center;">1</td>
+      <td style="text-align:center;"><span>2</span></td>
     </tr>
   </table>
   <table align="center" width="100%" cellpadding="0" cellspacing="0" id="tabla_ordenacondicionar">
@@ -238,7 +246,91 @@
     <tr><td colspan="13" style="font-size:10px;height:70px;">{observaciones}</td></tr>
   </table>
   <div class="saltarpagina"> </div>
-  <canvas id="canvas" width="1200" height="900"></canvas>
+	<table align="center" width="100%" cellpadding="0" cellspacing="0" id="tabla_ordenacondicionar">
+    <tr>
+      <td rowspan="5" style="border:none;width:20%;text-align:center;">
+        <img src="integrado/imagenes/cliente.png" width="79" height="37" />
+      </td>
+    </tr>
+    <tr>
+      <td colspan="5" style="text-align:center;border-bottom:none;font-size:14px;">
+        <b>ORDEN DE ACONDICIONAMIENTO No. {codigo_operacion}</b>
+      </td>
+      <td rowspan="5" style="width:20%;text-align:center;">
+        <img src="integrado/imagenes/bysoft.png" width="79" height="37" />
+      </td>
+    </tr>
+    <tr>
+      <th><b>C&oacute;digo</b></th>
+      <th><b>Versi&oacute;n</b></th>
+      <th colspan="3"><b>P&aacute;gina</b></th>
+    </tr>
+    <tr>
+      <td style="text-align:center;">{codigo_reporte}</td>
+      <td></td>
+      <td style="text-align:center;"><span>2</span></td>
+      <td style="text-align:center;">de</td>
+      <td style="text-align:center;"><span>2</span></td>
+    </tr>
+  </table>
+  <table align="center" width="100%" cellpadding="0" cellspacing="0" id="tabla_ordenacondicionar">
+    <tr>
+      <th colspan="8"><b>INFORMACI&Oacute;N GENERAL</b></th>
+    </tr>
+    <tr>
+      <td style="width: 12%;" class="tituloForm">No. Orden</td>
+      <td style="width: 13%"><b>{orden}</b></td>
+      <td class="tituloForm" style="width: 12%">Fecha de Operaci&oacute;n</td>
+      <td style="width: 13%">{fecha}</td>
+      <td class="tituloForm" style="width: 12%">Pedido</td>
+      <td style="width: 13%"><b>{pedido}</b></td>
+      <td class="tituloForm" style="width: 8%">No. Gu&iacute;a</td>
+      <td style="width: 17%"></td>
+    <tr>
+      <td style="width: 10%;" class="tituloForm">Nombre Cliente</td>
+      <td style="width: 15%"><b>{razon_social}</b></td>
+      <td class="tituloForm" style="width: 12%">Documento Cliente</td>
+      <td style="width: 13%">
+        {numero_documento}
+        <input type="hidden" name="doc_cliente" id="doc_cliente" value="{numero_documento}" />
+      </td>
+      <td class="tituloForm" style="width: 12%">Destinatario</td>
+      <td style="width: 13%">{destinatario}</td>
+      <td style="width: 8%;" class="tituloForm">Ciudad / Direcci&oacute;n</td>
+      <td style="width: 17%"><b>{ciudad} / {direccion}</b></td>
+    </tr>      
+    <tr>      
+      <td class="tituloForm" style="width: 12%">Conductor</td>
+      <td style="width: 13%">
+        {conductor_nombre}
+      </td>
+      <td class="tituloForm" style="width: 12%">Identificaci&oacute;n</td>
+      <td style="width: 13%">{conductor_identificacion}</td>
+      <td class="tituloForm" style="width: 12%">Placa</td>
+      <td style="width: 13%">{placa}</td>
+      <td class="tituloForm" style="width: 8%">FMM</td>
+      <td style="width: 17%">{fmm}</td>
+    </tr>
+    <tr>
+      <td class="tituloForm" style="width: 12%">Registro INVIMA</td>
+      <td style="width: 13%">{reginvima}</td>
+      <td class="tituloForm" style="width: 12%">Tipo de Mercancia</td>
+      <td style="width: 13%">{nombre_tipo_mercancia}</td>
+      <td class="tituloForm" style="width: 12%">Cantidad</td>
+      <td style="width: 13%">{cantidad}</td>
+      <td class="tituloForm" style="width: 8%">Cantidad Nacional</td>
+      <td style="width: 1%">{cantidad_nac}</td>
+    </tr>
+    <tr>
+      <td class="tituloForm" style="width: 12%">Cantidad Extranjera</td>
+      <td style="width: 13%">{cantidad_ext}</td>
+      <td class="tituloForm" style="width: 12%">Peso</td>
+      <td style="width: 13%">{peso}</td>
+      <td class="tituloForm" style="width: 12%">Valor</td>
+      <td colspan="3" style="width: 13%">{valor}</td>
+    </tr>
+  </table>
+  <canvas id="canvas" width="1200" height="710"></canvas>
   <script>
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext('2d');
@@ -282,17 +374,25 @@
       }
     }      
     ctx.lineWidth = 4;
-    var nrect = 1;
+    var nrect = 0;
     var registros = {nr};
     var rectWidth = 100;
-    for(i=10;i<=200;i=i+170) {
-      ctx.fillStyle = "#000";
-      ctx.fillText("ETIQUETA-"+nrect,(i+rectWidth)/2,10);
-      ctx.fillStyle = "#FFF";
-      roundRect(ctx, i, 15, 150, 50, 10, true);
-      if(nrect==registros) break;
-      nrect++;rectWidth+=170;
-    }
+		var colY = 10;
+		var ne = 1;
+		for(rectY=15;rectY<=(10*70);rectY=rectY+70) {
+			for(rectX=10;rectX<=(7*170);rectX=rectX+170) {
+				ctx.fillStyle = "#000";
+				ctx.fillText("ETIQUETA-"+ne,(rectX+rectWidth)/2,colY);
+				ctx.fillStyle = "#FFF";
+				roundRect(ctx, rectX, rectY, 150, 50, 10, true);
+				nrect++;
+				if(nrect==registros) break;
+				ne++;
+				rectWidth+=170;
+			}
+			if(nrect==registros) break;
+			colY+=70;rectWidth = 100;
+		}
   </script>
 </body>
 </html>
