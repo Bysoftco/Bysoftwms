4509010abfc304a51b892805dfc747cf9ce8f6f9
Bysoftco <32655676+Bysoftco@users.noreply.github.com>
2024-02-29 11:16:48 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
-header('Content-Type: text/html; charset=ISO-8859-1');
+header('Content-Type: text/html; charset=UTF-8');
 session_start();
+ini_set("include_path",'/home1/isamis/php:.:/opt/cpanel/ea-php56/root/usr/share/pear' . ini_get("include_path") );
 define('entrada_valida', true);
 require_once('config.php');
-require_once(LIB_PATH.'HTML_Template_IT/IT.php');
+require_once('HTML/Template/IT.php');
 require_once(CLASSES_PATH.'DatabaseMySQL.php');
 require_once(CLASSES_PATH.'adicionales.php');
 
 //LOCAL
-$_SESSION['conexion'] = new Database(array('DB_HOST'=>'localhost','DB_SOCK'=>'','DB_USER'=>'root','DB_PASS'=>'bysoft','DB_NAME'=>'nbysoft_db'));
+//$_SESSION['conexion'] = new Database(array('DB_HOST'=>'localhost','DB_SOCK'=>'','DB_USER'=>'root','DB_PASS'=>'73125365@_','DB_NAME'=>'nbysoftdb'));
 
 //SERVIDOR
-//$_SESSION['conexion'] = new Database(array('DB_HOST'=>'grupobysoft.com','DB_SOCK'=>'','DB_USER'=>'isamis_uwbysoft','DB_PASS'=>'pwbysoft','DB_NAME'=>'isamis_wmsbysoft'));
+$_SESSION['conexion'] = new Database(array('DB_HOST'=>'grupobysoft.com','DB_SOCK'=>'','DB_USER'=>'isamis_ulines','DB_PASS'=>'plines','DB_NAME'=>'isamis_linedb'));
 
 $template_file = 'aplicacion.php';
 $component = isset( $_REQUEST['component'] ) ? $_REQUEST['component'] : 'home';
--- index_blank.php
+++ index_blank.php
@@ -1,18 +1,18 @@
 <?php
-header('Content-Type: text/html; charset=ISO-8859-1');
 session_start();
 define('entrada_valida', true);
+//Configuración PEAR Sitio Web
+ini_set("include_path",'/home1/isamis/php:.:/opt/cpanel/ea-php56/root/usr/share/pear' . ini_get("include_path") );
 require_once('config.php');
-require_once(LIB_PATH.'HTML_Template_IT/IT.php');
+require_once('HTML/Template/IT.php');
 require_once(CLASSES_PATH.'DatabaseMySQL.php');
 require_once(CLASSES_PATH.'adicionales.php');
 
 //LOCAL
-$_SESSION['conexion'] = new Database(array('DB_HOST'=>'localhost','DB_SOCK'=>'','DB_USER'=>'root','DB_PASS'=>'bysoft','DB_NAME'=>'nbysoft_db'));
+//$_SESSION['conexion'] = new Database(array('DB_HOST'=>'localhost','DB_SOCK'=>'','DB_USER'=>'root','DB_PASS'=>'73125365@_','DB_NAME'=>'nbysoftdb'));
 
 //SERVIDOR
-//$_SESSION['conexion'] = new Database(array('DB_HOST'=>'grupobysoft.com','DB_SOCK'=>'','DB_USER'=>'isamis_uwbysoft','DB_PASS'=>'pwbysoft','DB_NAME'=>'isamis_wmsbysoft'));
-
+$_SESSION['conexion'] = new Database(array('DB_HOST'=>'grupobysoft.com','DB_SOCK'=>'','DB_USER'=>'isamis_ulines','DB_PASS'=>'plines','DB_NAME'=>'isamis_linedb'));
 
 $component = isset( $_REQUEST['component'] ) ? $_REQUEST['component'] : 'menus';
 $method = isset( $_REQUEST['method'] ) ? $_REQUEST['method'] : 'armar_menu_principal';
@@ -22,7 +22,6 @@ if(!isset( $_SESSION['datos_logueo']['usuario'] ) && $component!='login'){
 	die();
 }
 $component_path = COMPONENTS_PATH . $component . '/' . $component . '.php';
-
 include $component_path;
 $clase = new $component();
 $clase->$method($_REQUEST);
